ceca06c2efe3cf05a240d9f52807b2783aef9764
Vlad <ricean.vlad7@gmail.com>
2022-10-06 09:04:53 +0300

    -almost finished (need to move info down)
    https://developer.android.com/codelabs/basic-android-kotlin-compose-business-card?authuser=1&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-1-pathway-3%3Fauthuser%3D1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-business-card#0

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.example.businesscard
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.android.animation.SegmentType
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -63,7 +65,8 @@ fun LogoNameTitle() {
         )
         Text(
             text = "Vlad Ricean",
-            fontSize = 32.sp
+            fontSize = 32.sp,
+            color = Color.White
         )
         Text(
             text = "Compose Developer! :O",
@@ -73,6 +76,57 @@ fun LogoNameTitle() {
             fontWeight = FontWeight.Bold,
             color = Color.Green
         )
+        PersonalInfo(
+            phoneNr = "0757123456",
+            share = "@AndroidDev",
+            email = "vlad@gmail.com",
+        )
+    }
+}
 
+@Composable
+fun ColumnScope.PersonalInfo(
+    phoneNr: String,
+    share: String,
+    email: String,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight(0.5f)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start,
+    ) {
+        InfoItem(icon = painterResource(id = R.drawable.ic_baseline_local_phone_24), text = phoneNr)
+        InfoItem(icon = painterResource(id = R.drawable.ic_baseline_share_24), text = share)
+        InfoItem(icon = painterResource(id = R.drawable.ic_baseline_email_24), text = email)
+    }
+}
+
+@Composable
+fun InfoItem(
+    icon: Painter,
+    text: String
+) {
+    Divider(
+        color = Color.LightGray,
+        thickness = 1.dp
+    )
+    Row(
+        modifier = Modifier.padding(
+            start = 50.dp,
+            top = 10.dp,
+            bottom = 10.dp
+        )
+    ) {
+        Image(
+            painter = icon,
+            contentDescription = null,
+        )
+        Text(
+            text = text,
+            color = Color.White,
+            modifier = Modifier.padding(start = 20.dp),
+        )
     }
 }
